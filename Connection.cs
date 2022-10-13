c268186537f0e8d2211960a1d4003c558cc45514
Ntwanan <115715895+Ntwanan@users.noreply.github.com>
2022-10-13 15:58:17 +0200

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Connection
+    {
+        public string getConnection= @"Data Source=MASHAPOLE\SQLEXPRESS;Initial Catalog=APPR;Integrated Security=True";
+    }
+}
